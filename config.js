60ba61554cce850ee56162511f94833222f5c166
Sydney TEXIER <sydney.texier@gmail.com>
2026-04-25 21:33:11 +0200

    feat: migration vers Supabase, gestion des dates ISO et sécurisation RLS

---
+++ config.js
@@ -0,0 +1,20 @@
+const SUPABASE_URL = "https://sndokqwlvftbtiwvrlos.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuZG9rcXdsdmZ0YnRpd3ZybG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMzU5MDUsImV4cCI6MjA5MjcxMTkwNX0.XmpohnwRzSh143aI6jLqA-B5EssBszRXQe4VUwOsHyU";
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Fonction universelle pour récupérer les données
+async function fetchAllData() {
+  const { data, error } = await supabaseClient
+    .from("cyber_security")
+    .select("*")
+    .limit(30000)
+    .order("datetime", { ascending: true });
+
+  if (error) {
+    console.error("Erreur Supabase:", error);
+    return [];
+  }
+  return data;
+}
